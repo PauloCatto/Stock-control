ca1e69e57022bf8eff972ea03435904f9222f5ec
paulohcatto@gmail.com <paulohcatto@gmail.com>
2024-08-03 18:43:15 -0300

    Update project

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ChartModule } from 'primeng/chart';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    ChartModule
+  ],
+  providers: [
+    CookieService,
+    MessageService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { RouterModule } from '@angular/router';
+import { DASHBOARD_ROUTES } from './dashboard.routing';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MessageService } from 'primeng/api';
+import { CookieService } from 'ngx-cookie-service';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(DASHBOARD_ROUTES),
+    SidebarModule,
+    ButtonModule,
+    ToastModule,
+    ToolbarModule,
+    CardModule,
+    SharedModule
+  ],
+  providers:[MessageService, CookieService]
+})
+export class DashboardModule { }
